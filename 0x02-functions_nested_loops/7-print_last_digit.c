485c07d210793cfd292e71e0f021b6b6e086d981
KOKULEBA <kokulebaleticia@yahoo.com>
2022-09-13 07:34:32 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,15 +9,14 @@
 */
 
 int print_last_digit(int n)
+{
 
- {
+n = n % 10;
+if (n < 0)
+n = -n;
 
- n = n % 10;
- if (n < 0)
- n = -n;
+_putchar(n + '0');
 
- _putchar(n + '0');
+return (n);
 
- return (n);
-
- }
+}
