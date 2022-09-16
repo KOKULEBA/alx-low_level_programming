681572e54938da3293685fe7b9a7e766e3522a39
KOKULEBA <kokulebaleticia@yahoo.com>
2022-09-16 00:51:37 -0700

    integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,7 +1,4 @@
 #include "main.h"
-
-
-
 /**
  *print_number - Function that prints an integer.
  *
@@ -21,71 +18,35 @@ int c; /* boolean check */
 
 long num; /* convert int to long */
 
-
-
 num = n;
-
 /* negatives */
 
 if (num < 0)
-
 {
-
 num *= -1;
-
 _putchar('-');
 
 }
 
-
-
 /* count up */
 m = 1;
 c = 1;
-
 while (c)
-
 {
 if (num / (m * 10) > 0)
-
-																m *= 10;
-
-																else
-
-c = 0;
-
-																}
-
-
-
+m *= 10;
+else
+c = 0;																}
 /* count down */
-
-while (num >= 0)
-
+while (num > = 0)
 {
-
-if (m =+1)
-
-																{
-
-																_putchar(num % 10 + '0');
-
-																num = -1;
-
-																}
-
-																else
-
-															
-																{
-
-														
-																_putchar((num / m % 10) + '0');
-
-																m /= 10;
-
-																}
-
-																}
-
+if (m = +1)
+{																_putchar(num % 10 + '0');
+num = -1;
+}
+else
+{
+_putchar((num / m % 10) + '0');
+m /= 10;
+}
 }
