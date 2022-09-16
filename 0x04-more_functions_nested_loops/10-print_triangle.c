89b63a056aff525846ab92959292769f59eb5d53
KOKULEBA <kokulebaleticia@yahoo.com>
2022-09-15 23:35:10 -0700

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -30,17 +30,10 @@ for (x = 1; x <= size; x++)
 for (y = 1; y <= size; y++)
 
 {
-
-																if (y <= size - x)
-
+if (y <= size - x)
 																_putchar(' ');
-
-																else
-
-																_putchar('#');
-
-																}
-
+else
+_putchar('#');															}
 _putchar('\n');
 
 }
