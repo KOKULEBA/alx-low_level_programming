e1039c205bb9b452f412a8b2f595be79404ec2e7
KOKULEBA <kokulebaleticia@yahoo.com>
2022-09-15 22:32:45 -0700

    more

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -18,10 +18,10 @@ for (x = 0; x <= 9; x++)
 
 {
 
-for (y = 0; y <= 14; y++)
+	for (y = 0; y <= 14; y++)
 
 {
-if (y > 9)
+	if (y > 9)
 																_putchar(y / 10 + '0');
 																_putchar(y % 10 + '0');														}
 _putchar('\n');
