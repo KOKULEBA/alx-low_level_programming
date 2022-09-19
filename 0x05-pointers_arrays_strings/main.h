c01af41e28bed6ad96c5fa9a1d92ee4211e1d568
KOKULEBA <kokulebaleticia@yahoo.com>
2022-09-19 02:19:01 -0700

    main

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
+
 #define MAIN_H
-#include <stdio.h>
+
+
 int _putchar(char c);
 
 void reset_to_98(int *n);
@@ -25,4 +27,4 @@ char *_strcpy(char *dest, char *src);
 
 int _atoi(char *s);
 
-#endif/*MAIN_H*/
+#endif /*MAIN_H*/
