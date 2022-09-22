dd632bea9497dff3a5956c119b6f9d63f468e0d6
KOKULEBA <kokulebaleticia@yahoo.com>
2022-09-21 22:42:10 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -26,9 +26,9 @@ p = &n;
 *- you are not allowed to use a
 *
 *- you are not allowed to modify p
-*          
+*
 *- only one statement
-*		     
+*
 *- you are not allowed to code anything else than this line of code
 *
 */
