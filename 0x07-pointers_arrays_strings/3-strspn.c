bd117e049c77fd6496079a4d8cd3401778e3a7cb
KOKULEBA <kokulebaleticia@yahoo.com>
2022-09-26 01:07:29 -0700

    string

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,18 +2,45 @@
 
 #include <stddef.h>
 
-
 /**
- *_strspn - gets length of prefix substring
- *
- *@s: string to check
- *
- *@accept: bytes composing prefix
- *
- *Return: integer length of substring
- *
- */
+*_strchr - locates char in string
+*
+*@s: pointer to string
+*
+*@c: char to locate
+*
+*
+*Return: pointer to first occurence of c in s
+*
+*/
 
+char *_strchr(char *s, char c)
+
+{
+	
+while (*s)
+			
+{
+					
+if (*s == c)
+								
+return (s);
+							
+s++;
+									
+}
+		
+return (NULL);
+			
+}
+
+
+/**                                                                                                                             
+ *_strspn - gets length of prefix substring
+ *@s: string to check
+ *@accept: bytes composing prefix
+ *Return: integer length of substring
+ */    
 unsigned int _strspn(char *s, char *accept)
 
 {
