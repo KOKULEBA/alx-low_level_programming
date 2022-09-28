cddd1edd6cf75b0e2b73fbffc418c5ff3c703d55
KOKULEBA <kokulebaleticia@yahoo.com>
2022-09-28 01:26:05 -0700

    identical

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,35 +16,43 @@ return (1);
 else if (*s1 == '\0' || *s2 == '\0')
 {
 if (*s1 == '\0' && *s2 == '*')
-return wildcmp(s1, ++s2);
+return (wildcmp(s1, ++s2));
 else if (*s1 == '*' && *s2 == '\0')
-return wildcmp(++s1, s2);
+return (wildcmp(++s1, s2));
 return (0);
 }
 if (*s1 == *s2)
 {
-return wildcmp(++s1, ++s2);
+return (wildcmp(++s1, ++s2));
 }
 else if (*s1 == '*')
 {
 if (*(s1 + 1) == '*')
-return wildcmp(++s1, s2);
+return (wildcmp(++s1, s2));
 else
 {
-return wildcmp(s1, findsrc(s2, *(s1 + 1), 0, 0) + s2);
+return (wildcmp(s1, findsrc(s2, *(s1 + 1), 0, 0) + s2));
 }
 }
 else if (*s2 == '*')
 {
 if (*(s2 + 1) == '*')
-return wildcmp(s1, ++s2);
+return (wildcmp(s1, ++s2));
 else
 {
-return wildcmp(s1 + findsrc(s1, *(s2 + 1), 0, 0), s2);
+return (wildcmp(s1 + findsrc(s1, *(s2 + 1), 0, 0), s2));
 }
 }
 return (0);
 }
+/**
+ *findscrc - identifying strings
+ *@c: character
+ *@s: string
+ *@i: integer 1
+ *@p: integer 2
+ *Return: 1 if strings can be considered identical, else 0
+ */
 int findsrc(char *s, char c, int i, int p)
 {
 if (*(s + i) == '\0')
