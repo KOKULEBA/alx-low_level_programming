5807e75343359639d2ec531b4d173e737646a63e
KOKULEBA <kokulebaleticia@yahoo.com>
2022-10-06 03:12:54 -0700

    memory block

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,7 +1,4 @@
 #include "main.h"
-
-
-
 /**
  *_realloc - reallocates a memory block
  *
@@ -14,62 +11,30 @@
  *Return: Pointer to new memory block, or NULL if error
  *
  */
-
-
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-
 {
-
 char *p, *copy;
-
 unsigned int i;
-
-
-
 if (new_size == old_size)
-
 return (ptr);
-
 if (ptr != NULL && new_size == 0)
-
 {
-
 free(ptr);
-
 return (NULL);
 }
-
 if (ptr == NULL)
-
 {
-
 p = malloc(new_size);
-
 if (p == NULL)
-
 return (NULL);
-
 return (p);
-
 }
-
-
-
 p = malloc(new_size);
-
 if (p == NULL)
-
 return (NULL);
-
 copy = ptr;
-
 for (i = 0; i < old_size; i++)
-
 p[i] = copy[i];
-
 free(ptr);
-
 return (p);
-
 }
