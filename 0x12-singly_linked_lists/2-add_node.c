cca3b0ee217550f5e6edfc63120d02ceef026fbc
KOKULEBA <kokulebaleticia@yahoo.com>
2022-10-24 00:49:42 -0700

    lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,51 +18,24 @@
  */
 
 list_t *add_node(list_t **head, const char *str)
-
 {
-
 list_t *new_node;
-
 int c;
-
-
-
 for (c = 0; dup_str[c] != '\0'; c++)
-
-							;
-
 new_node = malloc(sizeof(list_t));
-
 if (new_node == NULL)
-
 {
-
 free(new_node);
-
 return (NULL);
-
 }
-
 new_node->str = strdup(str);
-
 if (new_node->str == NULL)
-
 {
-
 free(new_node);
-
 return (NULL);
-
 }
-
 new_node->len = c;
-
 new_node->next = *head;
-
-
-
 *head = new_node;
-
 return (*head);
-
 }
