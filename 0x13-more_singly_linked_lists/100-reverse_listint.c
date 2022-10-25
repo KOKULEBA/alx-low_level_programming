91985d52612bb3ebbcb12785431f15956323f5dc
KOKULEBA <kokulebaleticia@yahoo.com>
2022-10-24 23:37:56 -0700

    reverse

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -18,57 +18,27 @@
  */
 
 listint_t *reverse_listint(listint_t **head)
-
 {
-
 listint_t *hold;
-
 listint_t *current;
-
-
-
 if (*head == NULL)
-
 return (NULL);
-
-
-
 current = *head;
-
 *head = current->next;
-
 hold = (*head)->next;
-
 current->next = NULL;
-
 if (*head == NULL)
-
 {
 *head = current;
-
 return (current);
-
 }
-
-
-
-while (hold != NUL)
+while (hold != NULL)
 {
-
 (*head)->next = current;
-
 current = *head;
-
 *head = hold;
-
 hold = (*head)->next;
-
 }
-
-
-
 (*head)->next = current;
-
 return (*head);
-
 }
