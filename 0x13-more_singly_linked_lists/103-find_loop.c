7ff0e2e9b49ffa0f49bff219bc21d62f793576e7
KOKULEBA <kokulebaleticia@yahoo.com>
2022-10-25 00:17:13 -0700

    loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -16,57 +16,25 @@
  */
 
 listint_t *find_listint_loop(listint_t *head)
-
 {
 listint_t *currents, *currentf;
-
-
-
 if (head == NULL)
-
 return (NULL);
-
-
-
 currents = currentf = head;
-
 do {
-
 if (currents->next)
-
 currents = currents->next;
-
 else
-
 return (NULL);
-
-
-
-if (currentf->next->next)
-
-																currentf = currentf->next->next;
+if (currentf->next->next)													currentf = currentf->next->next;
 else
-
 return (NULL);
-
 } while (currentf != currents);
-
-
-
 currents = head;
-
 while (currentf != currents)
-
 {
-
 currentf = currentf->next;
-
 currents = currents->next;
-
 }
-
-
-
 return (currents);
-
 }
